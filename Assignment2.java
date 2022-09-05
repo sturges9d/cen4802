1ec988d16126d2b183ccd4764e565feb8b88f57d
Stephen Sturges Jr <stephen.sturges.jr@outlook.com>
2022-09-05 13:49:10 -0400

    Replaced hard-coded n=10 with user input and validation.

--- Assignment2.java
+++ Assignment2.java
@@ -1,7 +1,37 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * Class for CEN 4802C-15913, Software Integration, Configuration, and Testing, Assignment 2.
+ * @author Stephen Sturges Jr
+ * @version 09/05/2022
+ */
 public class Assignment2 {
 
+    /**
+     * Main method for the Assignment2 class. Calls the recursiveFibSeq method and outputs to the console 
+     * a String with the nth term and value of the Fibonacci sequence. Output string changes ordinal indicator
+     * depending on the n value used.
+     * @param args
+     */
     public static void main(String[] args) {
-        int n = 10;
+        boolean goodInput = false;
+        int n = 0;
+        // Get user input.
+        Scanner input = new Scanner(System.in);
+        while (!goodInput) {
+            try {
+                System.out.print("Please enter a value for n: ");
+                n = input.nextInt();
+                goodInput = true;
+            } catch (InputMismatchException ime) {
+                input.nextLine(); // Clear input buffer.
+                System.out.println("INPUT ERROR: Please enter an integer value.");
+                goodInput = false;
+            }
+        }
+        input.close();
+        // Calculate Fibonacci value.
         long result = recursiveFibSeq(n);
 
         char[] inputValue = Integer.toString(n).toCharArray();
@@ -26,6 +56,12 @@ public class Assignment2 {
         
     }
 
+    /**
+     * Returns the nth term of the Fibonacci sequence, calculated recursivley.
+     * @author Stephen Sturges Jr
+     * @param n The nth term of the Fibonacci sequence.
+     * @return long value of the nth term of the Fibonacci sequence.
+     */
     public static long recursiveFibSeq(long n) {
         if (n == 1) {
             return 0;
