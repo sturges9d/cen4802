cf528867adf87199dc3391e8095917b99bc8f987
Stephen Sturges Jr <stephen.sturges.jr@outlook.com>
2022-09-05 13:59:29 -0400

    Added user input and validation.

--- Assignment2.java
+++ Assignment2.java
@@ -1,7 +1,26 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Assignment2 {
 
     public static void main(String[] args) {
-        int n = 10;
+        boolean goodInput = false;
+        int n = 0;
+        // Get user input.
+        Scanner input = new Scanner(System.in);
+        while (!goodInput) {
+            try {
+                System.out.print("Please enter a value for n: ");
+                n = input.nextInt();
+                goodInput = true;
+            } catch (InputMismatchException ime) {
+                input.nextLine(); // Clear input buffer.
+                System.out.println("INPUT ERROR: Please enter an integer value.");
+                goodInput = false;
+            }
+        }
+        input.close();
+        // Calculate Fibonacci value.
         long result = recursiveFibSeq(n);
 
         char[] inputValue = Integer.toString(n).toCharArray();
